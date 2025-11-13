a02bcd38de01dc5ba39f17a71015fccac11e0ceb
tiara <tiarardj787@gmail.com>
2025-11-13 13:56:42 -0400

    Setup GitHub Pages deployment with gh-pages

--- water-quality-pr/next.config.mjs
+++ water-quality-pr/next.config.mjs
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  /* config options here */
+  output: 'export', 
+  basePath: isProd ? '/water-qualituy-pr' : '',
+  assetPrefix: isProd ? '/water-qualituy-pr/' : '',  
 };
 
 export default nextConfig;
