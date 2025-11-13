b63c9bd02a9740919486b66fbb22bb39778312a6
tiara <tiarardj787@gmail.com>
2025-11-13 13:55:20 -0400

    Updates

---
+++ _next/static/GgVnCQeSUWG0PPeVUpkA5/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/water-qualituy-pr//_next/:path+",
+        "destination": "/water-qualituy-pr/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
